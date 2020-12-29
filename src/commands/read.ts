20bec841b3fcfe17d90f397a3bd2b4ec8af46bb0
Alexandre Kohen <alexandre@kohen.fr>
2020-12-29 17:47:32 +0100

    Read command throws error on missing file

--- src/commands/read.ts
+++ src/commands/read.ts
@@ -3,10 +3,10 @@ import { Command, Context } from "../typings";
 export const read:Command = {
   run: (ctx: Context, args) => {
     const currentNode = ctx.player.currentNode
-    if(currentNode.files != undefined && args != undefined) {
+    if(currentNode.files != undefined && args != undefined && currentNode.files[args]) {
       return currentNode.files[args]
     }
-    return "File not found"
+    throw new Error(`File ${args} not found`)
   },
   help: () => "Reads the content of a file"
 };
