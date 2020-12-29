780a0cf5011614937ac18d3487119db1ff111ca0
Alexandre Kohen <alexandre@kohen.fr>
2020-12-29 18:47:47 +0100

    Tutorial first stages

--- src/expeditions/tutorial.ts
+++ src/expeditions/tutorial.ts
@@ -1,12 +1,20 @@
 import { Expedition } from "../expedition";
 import { Context, Node } from "../typings";
+import { em } from "../utils";
 import { doc } from "./nodes/doc";
 import { locked } from "./nodes/locked";
 
 const nodes: {[idx: string]: Node} = {
   start: {
-    welcome:() => "Welcome to this tutorial.",
-    files: {foo:"bar", file2:`file2\nmulti-line content`},
+    welcome:() => `Welcome to this tutorial!\nType ${em('read how-to')} to start!`,
+    files: {'how-to':`In each expedition, you're trying to recover data from a ship before the timer runs out.
+You can use different commands to navigate the ship's systems.
+The command you've just used is ${em('read')} and allows you to open files by giving it the name of the file you want to open. 
+It this case, you've opened the 'how-to' file.
+To continue, you'll need to use the ${em('ls')} command to list the files on this system, then the ${em('read')} command to open it.`,
+next:`Let's go to another system using the ${em('connect')} command.
+The system we want to connect to is called 'doc'.
+If you need additional information, you can use ${em('help connect')} to display this command help page.`},
   },
 };
 
@@ -24,7 +32,7 @@ function create(): Expedition {
       },
     }})
     exp.nodes
-      .set('doc2', doc('Welcome',{'name': 'content'}))
+      .set('doc', doc('Welcome',{'name': 'content'}))
       .set('doc3', doc('Welcome',{'name': 'content'}))
     exp.commands
       .set('expedition-specific',{run:(ctx, args) => (args)})
