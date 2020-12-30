bf30011cbc7c89dbcf1e4dd3694dcb2fcdee52f5
Alexandre Kohen <alexandre@kohen.fr>
2020-12-30 12:06:53 +0100

    ls sorts files alphabetically

--- src/commands/ls.ts
+++ src/commands/ls.ts
@@ -3,8 +3,8 @@ import { Command, Context } from "../typings";
 export const ls:Command = {
   run: (ctx: Context) => {
     const currentNode = ctx.player.currentNode
-    if(currentNode.files != undefined) {
-      return `Available files:\n  ${Object.keys(currentNode.files).join("\n  ")}`
+    if(currentNode.files != undefined && Object.keys(currentNode.files).length > 0) {
+      return `Available files:\n  ${Object.keys(currentNode.files).sort((a,b) => (a.localeCompare(b))).join("\n  ")}`
     } else {
       return "No files"
     }
