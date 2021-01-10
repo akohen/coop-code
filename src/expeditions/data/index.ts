ccea736d1b5c45a93fc611330ac14d90b71ebb2c
Alexandre Kohen <alexandre@kohen.fr>
2021-01-10 13:10:46 +0100

    SampleData is exported and can shuffle

--- src/expeditions/data/index.ts
+++ src/expeditions/data/index.ts
@@ -1,21 +1,23 @@
 import { files } from "./files"
 import { fakeWords, fakeWordsLong } from "./passwords"
+import { userNames } from "./users";
 
-class SampleData<T> extends Array<T> {
-  sample(len = 1) {
-    const shuffled = [...this]
+export class SampleData<T> extends Array<T> {
+  shuffled():SampleData<T> {
+    const shuffled = new SampleData(...this)
     for (let i = shuffled.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
-    return shuffled.slice(0,len)
+    return shuffled
   }
-
-  random() { return this[Math.floor(Math.random()*this.length)] }
-  static from<T>(arr: Iterable<T> | ArrayLike<T>) { return new SampleData(...Array.from(arr)) }
+  sample(len = 1):SampleData<T> { return new SampleData(...this.shuffled().slice(0,len)) }
+  random():T { return this[Math.floor(Math.random()*this.length)] }
+  static from<T>(arr: Iterable<T> | ArrayLike<T>):SampleData<T> { return new SampleData(...Array.from(arr)) }
 }
 
 export const sampleData = {
   logs: SampleData.from(files.logs),
-  passwords: {fakeWords:new SampleData(...fakeWords), fakeWordsLong: new SampleData(...fakeWordsLong)}
+  passwords: {fakeWords:new SampleData(...fakeWords), fakeWordsLong: new SampleData(...fakeWordsLong)},
+  users: SampleData.from(userNames)
 }
